aee83ed303970d2224008785f2bb4f2c7f8a4305
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-07-28 02:20:29 +0530

    initial commit
    business code, junit test code, mockito test code written

--- src/main/java/com/db/ridebooking/repository/DbUtil.java
+++ src/main/java/com/db/ridebooking/repository/DbUtil.java
@@ -5,8 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
-    private static final String URL = "jdbc:postgresql://localhost:5432/ridebooking";
-    private static final String USER = "postgres";
+
+//    PostgreSQL properties
+//    private static final String URL = "jdbc:postgresql://localhost:5432/ridebooking";
+//    private static final String USER = "postgres";
+//    private static final String PASSWORD = "root";
+
+//  MySQL properties
+    private static final String URL = "jdbc:mysql://localhost:3306/ridebooking";
+    private static final String USER = "root";
     private static final String PASSWORD = "root";
 
     public static Connection getDbConnection() throws SQLException {
